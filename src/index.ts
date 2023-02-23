a098fef6db81ea7ffcdc5258282b1289d8dceee5
Koushik Dey <koushik.dey@live.com>
2023-02-23 20:20:57 +0200

    solved issues with abstract classes

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,19 @@ import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 //const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-//const sorter = new Sorter(numbersCollection);
-//sorter.sort();
+//numbersCollection.sort();
 //console.log(numbersCollection.data);
 
 //const charactersCollection = new CharactersCollection('Xaaayb');
-//const sorter = new Sorter(charactersCollection);
-//sorter.sort();
+//charactersCollection.sort();
 //console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
-linkedList.add(0);
+linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
 
